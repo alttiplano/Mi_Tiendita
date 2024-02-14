17ae8a85bc9d2ba2cd9bda534994f71b84528a6e
bastndev <gohit24@gmail.com>
2024-02-14 17:18:53 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/pages/auth/login/login_page.dart
+++ lib/src/pages/auth/login/login_page.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page1 '),
-      )
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/images/login.svg',
+            fit: BoxFit.cover,
+          ),
+          const SafeArea(
+              child: Center(
+            child: Text(
+              'Login Page',
+              style: TextStyle(color: Colors.white),
+            ),
+          )),
+        ],
+      ),
     );
   }
 }
