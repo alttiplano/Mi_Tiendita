873604fb8b12e9b1bf7f386af48502dcc7fab501
bastndev <gohit24@gmail.com>
2024-02-14 19:09:57 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/pages/auth/login/login_card.dart
+++ lib/src/pages/auth/login/login_card.dart
@@ -7,11 +7,29 @@ class LoginCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: const BoxDecoration(
         color: CardColor.card,
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(40),
           topRight: Radius.circular(40),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Log in',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
