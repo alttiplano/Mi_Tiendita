65e37dccb20b061ec3571f00d838aea4945015e3
bastndev <gohit24@gmail.com>
2024-02-15 15:43:10 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/pages/login/auth/otp_phone.dart
+++ lib/src/pages/login/auth/otp_phone.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpPhone extends StatelessWidget {
   const OtpPhone({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 60,
+      textStyle: const TextStyle(
+        fontSize: 22,
+        color: Colors.black,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.green.shade100,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.transparent)),
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('OTP Phone'),
@@ -12,7 +25,18 @@ class OtpPhone extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
-        child: Container(),
+        child: Container(
+          child: Pinput(
+            length: 4,
+            defaultPinTheme: defaultPinTheme,
+            focusedPinTheme: defaultPinTheme.copyWith(
+              decoration: defaultPinTheme.decoration!.copyWith(
+                border: Border.all(color: Colors.green),
+              ),
+            ),
+            onCompleted: (pin) => debugPrint(pin),
+          ),
+        ),
       ),
     );
   }
