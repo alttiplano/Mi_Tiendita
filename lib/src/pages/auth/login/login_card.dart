3e31eacf9372d5a689724dc761858b88f240f1a2
bastndev <gohit24@gmail.com>
2024-02-15 11:15:08 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/pages/auth/login/login_card.dart
+++ lib/src/pages/auth/login/login_card.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mi_tiendita/src/pages/auth/login/themes/colors.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:mi_tiendita/src/pages/auth/login/widgets/continue_line.dart';
+import 'package:mi_tiendita/src/pages/auth/login/widgets/sign_button.dart';
 import 'package:mi_tiendita/src/pages/auth/login/widgets/social_sign.dart';
-import 'package:mi_tiendita/src/themes/button_color.dart';
 
 class LoginCard extends StatelessWidget {
   const LoginCard({Key? key}) : super(key: key);
@@ -19,12 +19,12 @@ class LoginCard extends StatelessWidget {
           topRight: Radius.circular(40),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
+      child: const Padding(
+        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               'Inicar Sesión',
               style: TextStyle(
                   color: Colors.black,
@@ -32,10 +32,10 @@ class LoginCard extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                   fontFamily: 'Poppins'),
             ),
-            const SizedBox(height: 40),
+            SizedBox(height: 40),
             Column(
               children: [
-                const IntlPhoneField(
+                IntlPhoneField(
                   decoration: InputDecoration(
                     labelText: 'Numero',
                     border: OutlineInputBorder(
@@ -45,30 +45,13 @@ class LoginCard extends StatelessWidget {
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ButtonColor.button,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                  ),
-                  child: const Text(
-                    'Iniciar Sesión',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                ), // Add your text here
+              SignButton()// Add your text here
               ],
             ),
-            const SizedBox(height: 25),
-            const ContinueLine(),
-            const SizedBox(height: 15),
-            const Row(
+            SizedBox(height: 25),
+            ContinueLine(),
+            SizedBox(height: 15),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SquareTile(imagePath: 'assets/images/google.png'),
---
+++ lib/src/pages/auth/login/widgets/sign_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mi_tiendita/src/themes/button_color.dart';
+
+class SignButton extends StatelessWidget {
+  const SignButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: ButtonColor.button,
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13),
+            ),
+          ),
+          child: const Text(
+            'Iniciar Sesión',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Poppins',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
